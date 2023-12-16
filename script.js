f44514ee473adc5105a62708df91a692dc4b3eac
unknown <lateefahbaba123@gmail.com>
2023-12-16 20:03:00 +0530

    final

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
-const form=document.getElementById("Todo_input_form");
-const createButton=document.getElementById("form_button");
+const form = document.getElementById("Todo_input_form");
+const createButton = document.getElementById("form_button");
 // console.log(form);
-const listCount=document.getElementById("list_count");
-let count=0;
-listCount.innerText=count;
-const todoElement=document.getElementById("todo");
-const contentBox=document.getElementById("todo_list_container");
-const emptyele=document.getElementById("empty_container");
-const onsubmitform=(event)=>{
-    event.preventDefault();
-    count++;
-    listCount.innerText=count;
-    console.log(listCount);
-    emptyele.className="clip_bord";
-    todoElement.innerText="To-do List";
-    // console.log(todoElement);
-    const record=document.createElement("div");
-    record.id="todo_list_content";
-    const para=document.createElement("p");
-    para.innerText=event.target.content.value;
-    const deletebtn=document.createElement("button");
-    const btnicon=document.createElement("span");
-    btnicon.className="material-symbols-outlined";
-   btnicon.innerText="delete";
-   deletebtn.appendChild(btnicon);
-    record.append(para,deletebtn);
-    contentBox.appendChild(record);
-    form.reset();
-    deletebtn.addEventListener("click",deleteRecord);
-    console.log(count);
-}
-function deleteRecord(event)
-{
-    count--;
-    listCount.innerText=count;
-    const deletebutton=event.target;
-    const record=deletebutton.parentNode.parentNode;
-    record.remove();
+const listCount = document.getElementById("list_count");
+let count = 0;
+listCount.innerText = count;
+const todoElement = document.getElementById("todo");
+const contentBox = document.getElementById("todo_list_container");
+const emptyele = document.getElementById("empty_container");
+const onsubmitform = (event) => {
+  event.preventDefault();
+  count++;
+  listCount.innerText = count;
+  console.log(listCount);
+  emptyele.className = "clip_bord";
+  todoElement.innerText = "To-do List";
+  // console.log(todoElement);
+  const record = document.createElement("div");
+  record.id = "todo_list_content";
+  const para = document.createElement("p");
+  para.innerText = event.target.content.value;
+  const deletebtn = document.createElement("button");
+  const btnicon = document.createElement("span");
+  btnicon.className = "material-symbols-outlined";
+  btnicon.innerText = "delete";
+  const listcheckbox = document.createElement("input");
+  listcheckbox.setAttribute("type", "checkbox");
+
+  deletebtn.appendChild(btnicon);
+  record.append(listcheckbox, para, deletebtn);
+  contentBox.appendChild(record);
+  form.reset();
+  deletebtn.addEventListener("click", deleteRecord);
+  // console.log(count);
+};
+function deleteRecord(event) {
+  count--;
+  listCount.innerText = count;
+  const deletebutton = event.target;
+  const record = deletebutton.parentNode.parentNode;
+  record.remove();
 }
 
-form.addEventListener("submit" ,onsubmitform);
+form.addEventListener("submit", onsubmitform);
